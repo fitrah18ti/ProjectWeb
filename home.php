1e04a8943364ff93a8c8462b00d66ccebd228da2
aness17 <tri18ti@mahasiswa.pcr.ac.id>
2019-07-14 21:59:51 +0700

    home
    ini menu utama

---
+++ home.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Shop Item - Start Bootstrap Template</title>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/shop-item.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Welcome to our Wedding Organizer</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tentang.html">Tentang Kami</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="layanan.html">Layanan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="galeri.html">Galeri</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Kontak</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="logout.php">Log out</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-3">
+        <h1 class="my-4">Anik Wedding Galery</h1>
+        <div class="list-group">
+
+        </div>
+      </div>
+
+      <div class="col-lg-9">
+
+        <div class="card mt-4">
+          <img class="card-img-top img-fluid" src="pelaminan1.jpeg" alt="">
+          <div class="card-body">
+            <h3 class="card-title">Pelaminan Nasional</h3>
+
+            <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
+            4.0 stars
+            <a href="kategori1.html">
+             <button type="submit" name="detail" value="detail" autofocus="" href="kategori1.html">Detail</button>
+           </div>
+         </div>
+
+         <div class="card mt-4">
+          <img class="card-img-top img-fluid" src="pelaminan2.jpg" alt="pelaminan2">
+          <div class="card-body">
+            <h3 class="card-title">Pelaminan Lux</h3>
+            
+            <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9733;</span>
+            5.0 stars
+            <a href="kategori2.html">
+              <button type="submit" name="detail" value="detail" autofocus="" href="kategori2.html">Detail</button>
+            </div>
+          </div>
+
+          <div class="card mt-4">
+            <img class="card-img-top img-fluid" src="pelaminan3.jpg" alt="pelaminan1">
+
+            <div class="card-body">
+              <h3 class="card-title">Pelaminan Luxury</h3>
+
+
+              <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9733;</span>
+              5.0 stars
+              <a href="kategori3.html">
+               <button type="submit" name="detail" value="detail" autofocus="" href="kategori3.html">Detail</button>
+             </div>
+           </div>
+           
+        </div>
+
+      </div>
+
+    </div>
+
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      </div>
+    </footer>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+  </html>
