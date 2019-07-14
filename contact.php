4ac7328f9c94bd5879f42fb808286be505e57fea
aness17 <tri18ti@mahasiswa.pcr.ac.id>
2019-07-14 22:59:31 +0700

    contact
    ini menu kontak

---
+++ contact.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Kontak Kami</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content=" Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	
+	<link rel="stylesheet" href="css/bootstrap.css"> 
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> 
+	<link rel="stylesheet" href="css/font-awesome.min.css"> 
+	<link href="//fonts.googleapis.com/css?family=Inconsolata:400,700&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+
+</head>
+
+<body>
+
+<header>
+	<div class="container">
+		<section class="row top_header pt-3">
+			<div class="col-lg-6 buttons ml-auto">
+				<p><span class="fa fa-phone"></span> +628126814387</p>
+				
+			</div>
+		</section>
+
+		<nav class="py-3">
+        <div id="logo">
+			<h1>
+				<a class="navbar-brand" href="home.php"> <span class="fa fa-empire"></span>Wedding <span><span class="line"></span>Organizer</span>
+				</a>
+			</h1>
+		</div>
+
+        <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+        <input type="checkbox" id="drop" />
+            <ul class="menu mt-2">
+                <li class="mr-lg-3 mr-2 active"><a href="home.php">Home</a></li>
+                <li class="mr-lg-3 mr-2"><a href="tentang.html">Tentang Kami</a></li>
+                <li class="mr-lg-3 mr-2"><a href="layanan.html">Layanan</a></li>
+                <li class="mr-lg-3 mr-2"><a href="galeri.html">Galeri</a></li>
+                <li class="mr-lg-3 mr-2 pb-0">
+               
+                <li class="active"><a href="contact.html">Kontak</a></li>
+            </ul>
+        </nav>
+	</div>
+</header>
+
+    <div class="inner_banner layer" id="home">
+        <div class="container">
+            <div class="agileinfo-inner">
+                <h2 class="text-center text-white">
+                    Kontak Person
+                </h2>
+            </div>
+        </div>
+    </div>
+    <!-- //inner banner -->
+    <!-- breadcrumbs -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="home.php">Home</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">Kontak Kami</li>
+        </ol>
+    </nav>
+    
+<section class="contact py-5">
+	<div class="container py-sm-3">
+		<h3 class="heading text-capitalize mb-lg-5 mb-4"> Kontak Kami </h3>
+		<div class="address row">
+			<div class="col-md-4 address-grid">
+				<div class="address-info">
+					<div class="address-left text-center">
+						<span class="fa fa-map-marker"></span>
+					</div>
+					<div class="address-right text-center">
+						<h6 class="ad-info text-uppercase mb-2">Alamat</h6>
+						<p> Jl. Tuanku tambusai, Tanah Putih, Rokan Hilir.
+
+						</p>
+					</div>
+				</div>
+
+			</div>
+			<div class="col-md-4 address-grid">
+				<div class="address-info">
+					<div class="address-left text-center">
+						<span class="fa fa-envelope"></span>
+					</div>
+					<div class="address-right text-center">
+						<h6 class="ad-info text-uppercase mb-2">Email</h6>
+							<a href="mailto:information@example.com"> anikwedding@gmail.com</a>
+							
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-4 address-grid">
+				<div class="address-info">
+					<div class="address-left text-center">
+						<span class="fa fa-phone"></span>
+					</div>
+					<div class="address-right text-center">
+						<h6 class="ad-info text-uppercase mb-2">HP</h6>
+						<p>+628126814387</p>
+						
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+<form action="prosespengunjung.php" method="post">
+  
+		<div class="form row py-5">
+			<div class="col-lg-6 contact-form">
+				<form action="#" method="post">
+					<div class="fields-grid">
+						<div>
+							<label>Tinggalkan pesan anda disini ya...</label><br>
+						</div>
+						<div class="styled-input agile-styled-input-top">
+							<input type="text" name="nama" required="">
+
+							<label>Nama</label>
+							<span></span>
+						</div> 
+						<div class="styled-input">
+							<input type="email" name="email" required="">
+							<label>Email</label>
+							<span></span>
+						</div>
+						<div class="styled-input">
+							<input type="tel" name="nohp" required="">
+							<label>No Hp</label>
+							<span></span>
+						</div>
+						<div class="styled-input">
+							<input type="tel" name="pesan" required="">
+							<label>Pesan</label>
+							<span></span>
+						</div>
+					</div>
+					<input type="submit" name="submit" value="Submit">
+				</form>
+			</div>
+			<div class="col-lg-6 mt-lg-0 mt-5">
+				<div class="bg-img">
+					<div class="contact-layer">
+						<h3 class="mb-3">Jangan ragu  menghubungi kami untuk menanyakan informasi</h3>
+						<p><i class="fas mr-2 fa-mobile-alt"></i> +628126814387</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+	</div>
+</section>	
+
+<footer>
+	<div class="container py-5">
+		<div class="row footer-gap">
+			<div class="col-lg-4 col-sm-6">
+				<h3 class="text-capitalize mb-3">Alamat</h3>
+				<address class="mb-0">
+					<p class=""><span class="fa fa-map-marker"></span> Jl. Tuanku Tambusai <br>Tanah Putih, Rokan Hilir.</p>
+					<p><span class="fa fa-clock-o"></span> Services : 10 a.m to 6 p.m</p>
+					<p><span class="fa fa-phone"></span> +628126814387</p>
+					<p><span class="fa fa-envelope-open"></span> <a href="mailto:info@example.com">anikwedding@gmail.com</a></p>
+				</address>
+			</div>
+			<div class="col-lg-4 col-sm-6 mt-lg-0 mt-sm-0 mt-4 p-md-0">
+				<h3 class="text-capitalize mb-4">On Instagram</h3>
+				<div class="images">
+					<a class="pr-2" href="#"> <img src="images/g1.jpg" alt="" class="img-fluid"/> </a>
+					<a class="pr-2" href="#"> <img src="images/a.jpg" alt="" class="img-fluid"/> </a>
+					<a class="pr-2" href="#"> <img src="images/b.jpg" alt="" class="img-fluid"/> </a>
+					<a class="pr-2" href="#"> <img src="images/c.jpg" alt="" class="img-fluid"/> </a>
+					<a class="pr-2" href="#"> <img src="images/d.jpg" alt="" class="img-fluid"/> </a>
+					<a class="mt-2 pr-2" href="#"> <img src="images/e.jpg" alt="" class="img-fluid"/> </a>
+					<a class="mt-2 pr-2" href="#"> <img src="images/f.jpg" alt="" class="img-fluid"/> </a>
+				
+				</div>	
+			</div>
+			<div class="col-lg-3 offset-lg-1 col-sm-6 mt-lg-0 mt-sm-5 mt-4">
+				<h3 class="text-capitalize mb-3"> Ikuti Kami</h3>
+				<p class="mb-4">Sosial Media Kami</p>
+				<ul class="social mt-lg-0 mt-3">
+					<li class="mr-1"><a href="https://www.facebook.com/anik.rosnaini"><span class="fa fa-facebook"></span></a></li>
+					<li class="mr-1"><a href="https://www.instagram.com/anikriaspengantin/"><span class="fa fa-instagram"></span></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="copyright pb-5 text-center">
+		<p>© 2018 Wedding Organizer. All Rights Reserved </p>
+	</div>
+</footer>
+</body>
+</html>
