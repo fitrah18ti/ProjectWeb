6091c28ff6bb86be46c22a469b5ad9ed9fdacb13
aness17 <tri18ti@mahasiswa.pcr.ac.id>
2019-07-16 12:18:13 +0700

    edit

--- home.php
+++ home.php
@@ -100,13 +100,13 @@
              </div>
            </div>
 
-        </div>
+         </div>
 
-      </div>
+       </div>
 
-    </div>
+     </div>
 
-    <footer class="py-5 bg-dark">
+     <footer class="py-5 bg-dark">
       <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
       </div>
