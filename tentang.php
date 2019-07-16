e7ea3deb5224f04b573885262097dbd6d7999fcd
aness17 <tri18ti@mahasiswa.pcr.ac.id>
2019-07-16 12:13:25 +0700

    update tentang
    menu tentang

--- tentang.php
+++ tentang.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Wedding Organizer </title>
+	<title>Wedding Organizer </title>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
@@ -27,206 +27,206 @@
 
 <body>
 
-<header>
-	<div class="container">
-		<section class="row top_header pt-3">
-			<div class="col-lg-6 buttons ml-auto">
-				<p><span class="fa fa-phone"></span> +628 1268 14387</p>
-				
+	<header>
+		<div class="container">
+			<section class="row top_header pt-3">
+				<div class="col-lg-6 buttons ml-auto">
+					<p><span class="fa fa-phone"></span> +628 1268 14387</p>
+					
+				</div>
+			</section>
+			
+			<nav class="py-3">
+				<div id="logo">
+					<h1>
+						<a class="navbar-brand" href="home.php"> <span class="fa fa-empire"></span>Wedding <span><span class="line"></span>Organizer</span>
+						</a>
+					</h1>
+				</div>
+
+				<label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+				<input type="checkbox" id="drop" />
+				<ul class="menu mt-2">
+					<li class="mr-lg-3 mr-2"><a href="home.php">Home</a></li>
+					<li class="mr-lg-3 mr-2 active"><a href="tentang.html">Tentang Kami</a></li>
+					<li class="mr-lg-3 mr-2"><a href="layanan.html">Layanan</a></li>
+					<li class="mr-lg-3 mr-2"><a href="galeri.html">Galeri</a></li>
+					<li class="mr-lg-3 mr-2"><a href="logout.php">Log Out</a></li>
+					<li class="mr-lg-3 mr-2 pb-0">
+						<!-- First Tier Drop Down -->
+						
+						<li><a href="contact.html">Kontak</a></li>
+					</ul>
+				</nav>
 			</div>
-		</section>
+		</header>
+
+		<div class="inner_banner layer" id="home">
+			<div class="container">
+				<div class="agileinfo-inner">
+					<h2 class="text-center text-white">
+						Tentang Kami
+					</h2>
+				</div>
+			</div>
+		</div>
 		
-		<nav class="py-3">
-        <div id="logo">
-			<h1>
-				<a class="navbar-brand" href="home.php"> <span class="fa fa-empire"></span>Wedding <span><span class="line"></span>Organizer</span>
-				</a>
-			</h1>
-		</div>
-
-        <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
-        <input type="checkbox" id="drop" />
-            <ul class="menu mt-2">
-                <li class="mr-lg-3 mr-2"><a href="home.php">Home</a></li>
-                <li class="mr-lg-3 mr-2 active"><a href="tentang.html">Tentang Kami</a></li>
-                <li class="mr-lg-3 mr-2"><a href="layanan.html">Layanan</a></li>
-                <li class="mr-lg-3 mr-2"><a href="galeri.html">Galeri</a></li>
-                <li class="mr-lg-3 mr-2"><a href="logout.php">Log Out</a></li>
-                <li class="mr-lg-3 mr-2 pb-0">
-                <!-- First Tier Drop Down -->
-               
-                <li><a href="contact.html">Kontak</a></li>
-            </ul>
-        </nav>
-	</div>
-</header>
-
-    <div class="inner_banner layer" id="home">
-        <div class="container">
-            <div class="agileinfo-inner">
-                <h2 class="text-center text-white">
-                    Tentang Kami
-                </h2>
-            </div>
-        </div>
-    </div>
-    
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">
-                <a href="home.php">Home</a>
-            </li>
-            <li class="breadcrumb-item active" aria-current="page">Tentang Kami</li>
-        </ol>
-    </nav>
-    
-<section class="welcome py-5" id="welcome">
-	<div class="container py-md-5">
-		<h3 class="heading text-capitalize text-center mb-lg-5 mb-4"> Rencanakan Hari Terbaikmu </h3>
-		<div class="row welcome-grids">
-			<div class="col-lg-4 mb-lg-0 mb-5">
-				<h4 class="left-heading">Our Wedding Story</h4>
-				<p class="mb-3">Kami merupakan organizer yang melayani dekorasi diacara apa saja.Anik Wedding Gallery, menyediakan segala fasilitas wedding, mulai dari pelaminan, tenda, photo atau video dan make up artist yang berkualitas.
-				Terbukti dari beberapa konsumer kami yang merasa pusa dan terkesan dengan wo kami.
-				Anda selanjutnya! </p>
-				<div class="welcome-button mt-4">
-					<a href="tentang.html">Selengkapnya</a>
-				</div>
-			</div>
-			<div class="col-lg-4 col-sm-6">
-				<div class="image1 mb-4">
-				<h4>Makeup</h4>
-					<p class="mt-3">Make up natural, bold maupun bride.</P>
-				</div>
-				<div class="image1">
-				<h4>Catering</h4>
-					<p class="mt-3">Menawarkan catering dengan berbagai macam menu.</P>
-				</div>
-			</div>
-			<div class="col-lg-4 col-sm-6 mt-sm-0 mt-4">
-				<div class="image1 mb-4">
-				<h4>Photo / Video </h4>
-					<p class="mt-3">Menyediakan Photo box dan video Syuting.</P>
-				</div>
-				<div class="image1">
-				<h4>Musik</h4>
-					<p class="mt-3">Berbagai hiburan mulai dari band dan acapella.</P>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="stats_test">
-	<div class="wthree-different-dot1 py-5">
-	<div class="container py-3">
-		<h3 class="heading text-capitalize mb-lg-5 mb-4">Testimony </h3>
-		<div class="row">
-			<div class="col-12 text-center inner_stat_wthree_agileits">
-				<div class="row px-sm-0 px-3">
-					<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid">
-						<p class="counter">450</p>
-						<h4>Wedding</h4>
+		<nav aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="home.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active" aria-current="page">Tentang Kami</li>
+			</ol>
+		</nav>
+		
+		<section class="welcome py-5" id="welcome">
+			<div class="container py-md-5">
+				<h3 class="heading text-capitalize text-center mb-lg-5 mb-4"> Rencanakan Hari Terbaikmu </h3>
+				<div class="row welcome-grids">
+					<div class="col-lg-4 mb-lg-0 mb-5">
+						<h4 class="left-heading">Our Wedding Story</h4>
+						<p class="mb-3">Kami merupakan organizer yang melayani dekorasi diacara apa saja.Anik Wedding Gallery, menyediakan segala fasilitas wedding, mulai dari pelaminan, tenda, photo atau video dan make up artist yang berkualitas.
+							Terbukti dari beberapa konsumer kami yang merasa pusa dan terkesan dengan wo kami.
+						Anda selanjutnya! </p>
+						<div class="welcome-button mt-4">
+							<a href="tentang.html">Selengkapnya</a>
+						</div>
 					</div>
-					<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid1">
-						<p class="counter">1000</p>
-						<h4>Pesta</h4>
-					</div>
-					<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid2">
-						<p class="counter">145</p>
-						<h4>Acara Lain</h4>
-					</div>
-					<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid3">
-						<p class="counter">986</p>
-						<h4>Pencari tahunan</h4>
-					</div>
-					<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid4">
-						<p class="counter">490</p>
-						<h4>Pesan diterima</h4>
-					</div>
-					<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid5">
-						<p class="counter">560</p>
-						<h4>Happy Clients</h4>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+					<div class="col-lg-4 col-sm-6">
+						<div class="image1 mb-4">
+							<h4>Makeup</h4>
+							<p class="mt-3">Make up natural, bold maupun bride.</P>
+							</div>
+							<div class="image1">
+								<h4>Catering</h4>
+								<p class="mt-3">Menawarkan catering dengan berbagai macam menu.</P>
+								</div>
+							</div>
+							<div class="col-lg-4 col-sm-6 mt-sm-0 mt-4">
+								<div class="image1 mb-4">
+									<h4>Photo / Video </h4>
+									<p class="mt-3">Menyediakan Photo box dan video Syuting.</P>
+									</div>
+									<div class="image1">
+										<h4>Musik</h4>
+										<p class="mt-3">Berbagai hiburan mulai dari band dan acapella.</P>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
 
-<section class="about py-5">
-	<div class="container py-sm-3">
-		<h3 class="heading text-capitalize mb-lg-5 mb-4"> Siapa Kami?</h3>
-		<div class="row about-grids">
-			<div class="col-lg-6">
-				<h4>Kami merupakan organizer yang melayani dekorasi diacara apa saja.</h4>
-				<p class="mb-3">Anik Wedding Gallery, menyediakan segala fasilitas wedding, mulai dari pelaminan, tenda, photo atau video dan make up artist yang berkualitas.
-				Terbukti dari beberapa konsumer kami yang merasa pusa dan terkesan dengan wo kami.
-				Anda selanjutnya! </p>
-			</div>
-			<div class="col-lg-6 mt-lg-0 mt-5">
-				<img src="images/satu.jpg" alt="" class="img-fluid"/>
-			</div>
-		</div>
-	</div>
-</section>
+						<section class="stats_test">
+							<div class="wthree-different-dot1 py-5">
+								<div class="container py-3">
+									<h3 class="heading text-capitalize mb-lg-5 mb-4">Testimony </h3>
+									<div class="row">
+										<div class="col-12 text-center inner_stat_wthree_agileits">
+											<div class="row px-sm-0 px-3">
+												<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid">
+													<p class="counter">450</p>
+													<h4>Wedding</h4>
+												</div>
+												<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid1">
+													<p class="counter">1000</p>
+													<h4>Pesta</h4>
+												</div>
+												<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid2">
+													<p class="counter">145</p>
+													<h4>Acara Lain</h4>
+												</div>
+												<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid3">
+													<p class="counter">986</p>
+													<h4>Pencari tahunan</h4>
+												</div>
+												<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid4">
+													<p class="counter">490</p>
+													<h4>Pesan diterima</h4>
+												</div>
+												<div class="col-lg-2 col-sm-4 col-6 stats_left counter_grid5">
+													<p class="counter">560</p>
+													<h4>Happy Clients</h4>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
 
-<section class="query">
-	<div class="wthree-different-dot1 py-5">
-		<div class="container py-sm-3">
-			<div class="row query-grids">
-				<div class="col-lg-5">
-					<h4 class="mb-3">Jangan Salah Pilih!</h4>
-					<p class="mb-3">Kami menawarkan sesuai dengan kemauan anda.
-					Mulai dari acara biasa sampai dengan super mewah. 
-				Jangan pernah salah dalam memilih Wedding Organizer anda. 
-			Pilihlah yang paling berkesan.</p>
-					<a href="contact.html">Kontak Kami</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+							<section class="about py-5">
+								<div class="container py-sm-3">
+									<h3 class="heading text-capitalize mb-lg-5 mb-4"> Siapa Kami?</h3>
+									<div class="row about-grids">
+										<div class="col-lg-6">
+											<h4>Kami merupakan organizer yang melayani dekorasi diacara apa saja.</h4>
+											<p class="mb-3">Anik Wedding Gallery, menyediakan segala fasilitas wedding, mulai dari pelaminan, tenda, photo atau video dan make up artist yang berkualitas.
+												Terbukti dari beberapa konsumer kami yang merasa pusa dan terkesan dengan wo kami.
+											Anda selanjutnya! </p>
+										</div>
+										<div class="col-lg-6 mt-lg-0 mt-5">
+											<img src="images/satu.jpg" alt="" class="img-fluid"/>
+										</div>
+									</div>
+								</div>
+							</section>
 
-<footer>
-	<div class="container py-5">
-		<div class="row footer-gap">
-			<div class="col-lg-4 col-sm-6">
-				<h3 class="text-capitalize mb-3">Alamat</h3>
-				<address class="mb-0">
-					<p class=""><span class="fa fa-map-marker"></span> Jl. Tuanku Tambusai <br>Tanah Putih, Rokan Hilir.</p>
-					<p><span class="fa fa-clock-o"></span> Buka : 10 a.m to 6 p.m</p>
-					<p><span class="fa fa-phone"></span> +628126814387</p>
-					<p><span class="fa fa-envelope-open"></span> <a href="mailto:info@example.com">anikwo.com</a></p>
-				</address>
-			</div>
-			<div class="col-lg-4 col-sm-6 mt-lg-0 mt-sm-0 mt-4 p-md-0">
-				<h3 class="text-capitalize mb-4">On Instagram</h3>
-				<div class="images">
-					<a class="pr-2" href="#"> <img src="images/a.jpg" alt="" class="img-fluid"/> </a>
-					<a class="pr-2" href="#"> <img src="images/b.jpg" alt="" class="img-fluid"/> </a>
-					<a class="pr-2" href="#"> <img src="images/c.jpg" alt="" class="img-fluid"/> </a>
-					<a class="pr-2" href="#"> <img src="images/d.jpg" alt="" class="img-fluid"/> </a>
-					
-					<a class="mt-2 pr-2" href="#"> <img src="images/e.jpg" alt="" class="img-fluid"/> </a>
-					<a class="mt-2 pr-2" href="#"> <img src="images/f.jpg" alt="" class="img-fluid"/> </a>
-					
-				</div>	
-			</div>
-			<div class="col-lg-3 offset-lg-1 col-sm-6 mt-lg-0 mt-sm-5 mt-4">
-				<h3 class="text-capitalize mb-3"> Ikuti Kami</h3>
-				<p class="mb-4">Sosial Media</p>
-				<ul class="social mt-lg-0 mt-3">
-					<li class="mr-1"><a href="https://www.facebook.com/anik.rosnaini"><span class="fa fa-facebook"></span></a></li>
-					<li class="mr-1"><a href="https://www.instagram.com/anikriaspengantin/"><span class="fa fa-instagram"></span></a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="copyright pb-5 text-center">
-		<p>© 2018 Wedding Organizer. All Rights Reserved<a href="http://www.W3Layouts.com" target="_blank"></a></p>
-	</div>
-</footer>
+							<section class="query">
+								<div class="wthree-different-dot1 py-5">
+									<div class="container py-sm-3">
+										<div class="row query-grids">
+											<div class="col-lg-5">
+												<h4 class="mb-3">Jangan Salah Pilih!</h4>
+												<p class="mb-3">Kami menawarkan sesuai dengan kemauan anda.
+													Mulai dari acara biasa sampai dengan super mewah. 
+													Jangan pernah salah dalam memilih Wedding Organizer anda. 
+												Pilihlah yang paling berkesan.</p>
+												<a href="contact.html">Kontak Kami</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
 
-</body>
-</html>
+							<footer>
+								<div class="container py-5">
+									<div class="row footer-gap">
+										<div class="col-lg-4 col-sm-6">
+											<h3 class="text-capitalize mb-3">Alamat</h3>
+											<address class="mb-0">
+												<p class=""><span class="fa fa-map-marker"></span> Jl. Tuanku Tambusai <br>Tanah Putih, Rokan Hilir.</p>
+												<p><span class="fa fa-clock-o"></span> Buka : 10 a.m to 6 p.m</p>
+												<p><span class="fa fa-phone"></span> +628126814387</p>
+												<p><span class="fa fa-envelope-open"></span> <a href="mailto:info@example.com">anikwo.com</a></p>
+											</address>
+										</div>
+										<div class="col-lg-4 col-sm-6 mt-lg-0 mt-sm-0 mt-4 p-md-0">
+											<h3 class="text-capitalize mb-4">On Instagram</h3>
+											<div class="images">
+												<a class="pr-2" href="#"> <img src="images/a.jpg" alt="" class="img-fluid"/> </a>
+												<a class="pr-2" href="#"> <img src="images/b.jpg" alt="" class="img-fluid"/> </a>
+												<a class="pr-2" href="#"> <img src="images/c.jpg" alt="" class="img-fluid"/> </a>
+												<a class="pr-2" href="#"> <img src="images/d.jpg" alt="" class="img-fluid"/> </a>
+												
+												<a class="mt-2 pr-2" href="#"> <img src="images/e.jpg" alt="" class="img-fluid"/> </a>
+												<a class="mt-2 pr-2" href="#"> <img src="images/f.jpg" alt="" class="img-fluid"/> </a>
+												
+											</div>	
+										</div>
+										<div class="col-lg-3 offset-lg-1 col-sm-6 mt-lg-0 mt-sm-5 mt-4">
+											<h3 class="text-capitalize mb-3"> Ikuti Kami</h3>
+											<p class="mb-4">Sosial Media</p>
+											<ul class="social mt-lg-0 mt-3">
+												<li class="mr-1"><a href="https://www.facebook.com/anik.rosnaini"><span class="fa fa-facebook"></span></a></li>
+												<li class="mr-1"><a href="https://www.instagram.com/anikriaspengantin/"><span class="fa fa-instagram"></span></a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="copyright pb-5 text-center">
+									<p>© 2018 Wedding Organizer. All Rights Reserved<a href="http://www.W3Layouts.com" target="_blank"></a></p>
+								</div>
+							</footer>
+
+						</body>
+						</html>
